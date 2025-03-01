8561fc1fd2770eaf830b78cb47bc603535a2d404
fidpro <fedor.zeyts@gmail.com>
2025-03-01 21:40:15 +0300

    i've committed file

--- main.py
+++ main.py
@@ -1,7 +1,32 @@
-def main():
-    print('My first git program')
-    print('And I change it every day')
+import sys
+from PyQt6 import uic
+from PyQt6.QtWidgets import QApplication, QWidget
+from PyQt6.QtGui import QPainter, QColor
+from PyQt6.QtCore import Qt
+import random
 
+class CircleDrawer(QWidget):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('UI.ui', self)
+        self.pushButton.clicked.connect(self.draw_circle)
+        self.circles = []
+
+    def draw_circle(self):
+        diameter = random.randint(10, 100)
+        x = random.randint(0, self.width() - diameter)
+        y = random.randint(0, self.height() - diameter)
+        self.circles.append((x, y, diameter))
+        self.update()
+    def paintEvent(self, event):
+        painter = QPainter(self)
+        painter.setBrush(QColor(Qt.yellow))
+        for circle in self.circles:
+            x, y, diameter = circle
+            painter.drawEllipse(x, y, diameter, diameter)
 
 if __name__ == '__main__':
-    main()
+    app = QApplication(sys.argv)
+    window = CircleDrawer()
+    window.show()
+    sys.exit(app.exec_())
